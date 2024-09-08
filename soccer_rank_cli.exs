bc80745a8baefca553087f3855bca99e03c0a4a4
Hai Dinh <dinhnguyenminhhai@gmail.com>
2024-09-09 00:15:09 +0700

    created soccer rank cli script

---
+++ soccer_rank_cli.exs
@@ -0,0 +1,22 @@
+defmodule SoccerRankCLI do
+  defp parse_args(args) do
+    {opts, _, _} =
+      OptionParser.parse(args,
+        aliases: [f: :input_path, o: :output_path, t: :filetype],
+        strict: [input_path: :string, output_path: :string, filetype: :string]
+      )
+
+    {opts[:input_path], opts[:output_path], opts[:filetype]}
+  end
+
+  def read_input(input) when input == nil,
+    do: IO.read(:stdio, :all) |> String.split("\n", trim: true)
+
+  def read_input(input), do: File.stream!(input) |> Stream.map(&String.trim/1)
+end
+
+{input_path, output_path, filetype} = parse_args(System.argv())
+
+lines = read_input(input_path)
+
+SoccerRank.invoke(lines, output_path, filetype)
